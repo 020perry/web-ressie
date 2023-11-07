26eb3375ef54362efe9263977a044d2239653a7e
020perry <Naciy70@gmail.com>
2023-11-07 01:52:08 +0100

    opgeknapt menu table front end in algemeen

--- resources/views/dashboard/menus.blade.php
+++ resources/views/dashboard/menus.blade.php
@@ -6,6 +6,13 @@
             <div class="overflow-x-auto">
 
 
+                <div x-cloak x-show.transition="showSuccessMessage" class="alert alert-success">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <span x-text="successMessage"></span>
+                </div>
+
         <table class="table w-full">
         <thead>
         <tr>
